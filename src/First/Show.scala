5e456445caea7266ffa5bbe748bf82e7de38a45f
PamodiRanmal <ranmalmendis8@gmail.com>
2022-03-22 12:29:39 +0530

    # Scala

--- src/First/Show.scala
+++ src/First/Show.scala
@@ -1,9 +1,18 @@
 object Show {
 
   def main(args: Array[String]) {
-
+//Escape Sequences
     //    println("Hello\tWorld");
-    println("Hello\nWorld");
+    //    println("Hello\nWorld");
+    //    print("Hello World")
+    //    print("Hello World")
+    //    println("Hello\\World");
+    //    println("Hello\" World");
+
+//Data Types
+
+
+
 
   }
 
