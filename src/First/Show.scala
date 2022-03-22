524bc1eef99e572c454fb65dcf06de60b0f416e7
PamodiRanmal <ranmalmendis8@gmail.com>
2022-03-22 12:29:39 +0530

    # Scala

--- src/First/Show.scala
+++ src/First/Show.scala
@@ -10,8 +10,21 @@ object Show {
     //    println("Hello\" World");
 
 //Data Types
+    //    var x=10L
+    //Without giving the data type
+    //    var x=10.23F
+    //    println(x)
+    //Unit data type is simillar to void. We use it when we don't return anything
 
 
+    //By giving the data type
+//    var variableName : DataType=value
+//    var number: Double=10
+//    var number: Boolean=true
+//    var number1:Int=10
+//    var number2:Int=20
+//    var sum:Int=number1+number2
+//    println(sum)
 
 
   }
