ecb517e06e1ae2eebf4eedbda9a472937c30f388
PamodiRanmal <ranmalmendis8@gmail.com>
2022-03-25 18:47:18 +0530

    // annonymous functions

--- src/Show.scala
+++ src/Show.scala
@@ -1,7 +1,7 @@
 object Show {
 
   def main(args: Array[String]) {
-//Escape Sequences
+    //Escape Sequences
     //    println("Hello\tWorld");
     //    println("Hello\nWorld");
     //    print("Hello World")
@@ -9,7 +9,7 @@ object Show {
     //    println("Hello\\World");
     //    println("Hello\" World");
 
-//Data Types
+    //Data Types
     //    var x=10L
     //Without giving the data type
     //    var x=10.23F
@@ -18,13 +18,13 @@ object Show {
 
 
     //By giving the data type
-//    var variableName : DataType=value
-//    var number: Double=10
-//    var number: Boolean=true
-//    var number1:Int=10
-//    var number2:Int=20
-//    var sum:Int=number1+number2
-//    println(sum)
+    //    var variableName : DataType=value
+    //    var number: Double=10
+    //    var number: Boolean=true
+    //    var number1:Int=10
+    //    var number2:Int=20
+    //    var sum:Int=number1+number2
+    //    println(sum)
 
 
     // Var vs Val
@@ -46,146 +46,185 @@ object Show {
     //    println(name)
 
     //If else and how it works in Scala
-//    println("Hey Sirisena! Enter your number ")
-//    var number=scala.io.StdIn.readInt()
-//    if(number>=10)
-//    {
-//      println("Number is greater than or equal to 10")
-//    }
-//    else{
-//      println("Number is lesser than 10")
-//    }
+    //    println("Hey Sirisena! Enter your number ")
+    //    var number=scala.io.StdIn.readInt()
+    //    if(number>=10)
+    //    {
+    //      println("Number is greater than or equal to 10")
+    //    }
+    //    else{
+    //      println("Number is lesser than 10")
+    //    }
 
-//    //Else if
-//        println("Hey Sirisena! Enter your marks ")
-//        var marks=scala.io.StdIn.readInt()
-//
-//        if(marks>=75 && marks<=100)
-//        {
-//          println("Your Grade is A")
-//        }
-//
-//        else if(marks>=65 && marks<75)
-//        {
-//          println("Your Grade is B")
-//        }
-//
-//        else if(marks>=55 && marks<65)
-//        {
-//          println("Your Grade is C")
-//        }
-//
-//        else if(marks>=35 && marks<55)
-//        {
-//          println("Your Grade is S")
-//        }
-//
-////        else if(marks>=0 && marks<35)
-////        {
-////          println("Your Grade is W")
-////        }
-//
-//        else
-//        {
-//          println("Your Grade is W")
-//
-//        }
+    //    //Else if
+    //        println("Hey Sirisena! Enter your marks ")
+    //        var marks=scala.io.StdIn.readInt()
+    //
+    //        if(marks>=75 && marks<=100)
+    //        {
+    //          println("Your Grade is A")
+    //        }
+    //
+    //        else if(marks>=65 && marks<75)
+    //        {
+    //          println("Your Grade is B")
+    //        }
+    //
+    //        else if(marks>=55 && marks<65)
+    //        {
+    //          println("Your Grade is C")
+    //        }
+    //
+    //        else if(marks>=35 && marks<55)
+    //        {
+    //          println("Your Grade is S")
+    //        }
+    //
+    ////        else if(marks>=0 && marks<35)
+    ////        {
+    ////          println("Your Grade is W")
+    ////        }
+    //
+    //        else
+    //        {
+    //          println("Your Grade is W")
+    //
+    //        }
 
-          //for loops
-//            for(i<-1 to 10)
-//            for(i<-1 until 10)
-//            {
-//              println("i = "+i)
-//              println("Ranmal Mendis")
-//            }
+    //for loops
+    //            for(i<-1 to 10)
+    //            for(i<-1 until 10)
+    //            {
+    //              println("i = "+i)
+    //              println("Ranmal Mendis")
+    //            }
 
-//Nested for loops
-//                for(i<-1 until 6)
-//                {
-//                  for(j<-1 until 6)
-//                  {
-//                    println("i = "+i +" j= "+j)
-//                  }
-//
-//                }
+    //Nested for loops
+    //                for(i<-1 until 6)
+    //                {
+    //                  for(j<-1 until 6)
+    //                  {
+    //                    println("i = "+i +" j= "+j)
+    //                  }
+    //
+    //                }
 
-//    print asterisk pattern
-//    for(i<-1 until 7)
-//    {
-//      print("* ")
-//
-//
-//      for(j<-1 until i)
-//      {
-//        print("* ")
-//
-//      }
-//      println()
-//
-//    }
+    //    print asterisk pattern
+    //    for(i<-1 until 7)
+    //    {
+    //      print("* ")
+    //
+    //
+    //      for(j<-1 until i)
+    //      {
+    //        print("* ")
+    //
+    //      }
+    //      println()
+    //
+    //    }
 
-   // While loop
-//   var number=0
-//
-//
-//
-//    while(number != 10){
-//      print("Enter your Number: ")
-//      number=scala.io.StdIn.readInt();
-//
-//    }
+    // While loop
+    //   var number=0
+    //
+    //
+    //
+    //    while(number != 10){
+    //      print("Enter your Number: ")
+    //      number=scala.io.StdIn.readInt();
+    //
+    //    }
 
     //Do while loop
-//        var number=0
-//
-//        do {
-//          print("Enter your Number: ")
-//          number=scala.io.StdIn.readInt()
-//        }
-//        while(number != 10)
-//        {
-////          print("Enter your Number: ");
-////          number=scala.io.StdIn.readInt();
-//          print(" ****** ")
-//
-//        }
+    //        var number=0
+    //
+    //        do {
+    //          print("Enter your Number: ")
+    //          number=scala.io.StdIn.readInt()
+    //        }
+    //        while(number != 10)
+    //        {
+    ////          print("Enter your Number: ");
+    ////          number=scala.io.StdIn.readInt();
+    //          print(" ****** ")
+    //
+    //        }
 
     // Another method to call functions
     //call by val
-   var i=0
+    //   var i=0
+    //
+    //    show{
+    //      i+=1
+    //      i
+    //    }
+    //
+    //
+    //  }
 
-    show{
-      i+=1
-      i
-    }
+    //functions
 
+    //function parameters - deleted
+    // passing multiple params - deleted
+    // typecasting - deleted
 
-  }
+    //
 
-  //functions
+    //  def show(x1:Int)
+    //  {
+    //    println(x1)
+    //    println(x1)
+    //    println(x1)
+    //
+    //
+    //  }
 
-  //function parameters - deleted
-  // passing multiple params - deleted
-  // typecasting - deleted
+    //call by name
+    //  def show(x1: =>Int)
+    //  {
+    //    println(x1)
+    //    println(x1)
+    //    println(x1)
+    //
+    //
+    //  }
+    //
+    //name arguments
 
-  //
+//    add(b=5,a=4)
 
-//  def show(x1:Int)
-//  {
-//    println(x1)
-//    println(x1)
-//    println(x1)
+    // annonymous functions
+    //Method 1
+//    var result=(a:Int,b:Int)=>a+b
+//    println(result(10,10))
 //
+//    //Method 2
+//    var result1=(_:Int)+(_:Int)
+//    println(result1(2,3))
+
+
+  //name arguments
+//  def add  (a:Int,b:Int)
+//  {
+//    println("a = "+a)
+//    println("b = "+b)
 //
 //  }
 
-  //call by name
-  def show(x1: =>Int)
-  {
-    println(x1)
-    println(x1)
-    println(x1)
-
+ // annonymous functions
 
   }
+
+  //Higher order functions
+  // We pass a function as a parameter and we can even return a function from them
+  def hExample(x=Int, y=Int)
+
+  def add(x:Int,y:Int):Int={
+    return x+y
+  }
+
+
+
+
+
 }
